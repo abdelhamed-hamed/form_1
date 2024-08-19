0e2d241e1efd656da6772eb307010124828bca0f
abdelhamed hamed <157960201+abdelhamed-hamed@users.noreply.github.com>
2024-08-19 14:08:45 +0300

    Add files via upload

---
+++ form_1/src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent },
+];
